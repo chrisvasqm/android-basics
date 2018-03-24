38e8f11f0b1dff282a6c5aba7205cb58979e70fa
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-24 17:41:58 -0400

    Add first quiz

---
+++ app/src/main/java/com/chrisvasqm/androidbasics/FirstQuizActivity.kt
@@ -0,0 +1,26 @@
+package com.chrisvasqm.androidbasics
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_first_quiz.*
+import org.jetbrains.anko.toast
+
+class FirstQuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_quiz)
+
+        btnNextFirst.setOnClickListener {
+            checkAnswer()
+        }
+    }
+
+    private fun checkAnswer() {
+        if (!radioAnyAmount.isChecked)
+            toast(R.string.wrong)
+        else
+            toast(R.string.correct)
+    }
+
+}
