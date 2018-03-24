a77d3558d283306dd867b621998f85a87dd93019
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-24 14:34:21 -0400

    Add collapsible toolbar

--- app/src/main/java/com/chrisvasqm/androidbasics/MainActivity.kt
+++ app/src/main/java/com/chrisvasqm/androidbasics/MainActivity.kt
@@ -2,11 +2,13 @@ package com.chrisvasqm.androidbasics
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 }
