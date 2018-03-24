5249c9b80a6877ff4a9e77652a148b9d9ac7bf63
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-24 15:05:57 -0400

    feature: Add 1 point to each team

--- app/src/main/java/com/chrisvasqm/androidbasics/MainActivity.kt
+++ app/src/main/java/com/chrisvasqm/androidbasics/MainActivity.kt
@@ -2,11 +2,23 @@ package com.chrisvasqm.androidbasics
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPlusOneA.setOnClickListener { plusOnePoints(scoreTeamA) }
+
+        btnPlusOneB.setOnClickListener { plusOnePoints(scoreTeamB) }
     }
+
+    private fun plusOnePoints(textView: TextView) {
+        val score = textView.text.toString().toInt() + 1
+        textView.text = score.toString()
+    }
+
 }
