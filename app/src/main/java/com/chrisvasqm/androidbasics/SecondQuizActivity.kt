81a189f9ca40d425a6c43c440f24bb55b87f90bf
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-30 16:58:57 -0400

    Add second quiz

---
+++ app/src/main/java/com/chrisvasqm/androidbasics/SecondQuizActivity.kt
@@ -0,0 +1,35 @@
+package com.chrisvasqm.androidbasics
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second_quiz.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
+class SecondQuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_quiz)
+
+        btnPreviousSecond.setOnClickListener { showFirstQuiz() }
+        btnNextSecond.setOnClickListener {
+            checkAnswer()
+        }
+    }
+
+    private fun showFirstQuiz() {
+        startActivity<FirstQuizActivity>()
+    }
+
+    private fun checkAnswer() {
+        if (checkEncapsulation.isChecked
+                and checkPolymorphism.isChecked
+                and checkInheritance.isChecked
+                and checkAbstraction.isChecked)
+            toast(R.string.correct)
+        else
+            toast(R.string.wrong)
+    }
+
+}
