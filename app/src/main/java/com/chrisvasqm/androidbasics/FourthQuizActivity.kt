e53ae18d6cdab098ea07438c9d50054ab5896139
ISABELLA\Khrixuz <christianvasquezmelo@gmail.com>
2021-03-28 14:05:24 -0400

    Fix view dependencies issues for the Fourth Quiz screen

--- app/src/main/java/com/chrisvasqm/androidbasics/FourthQuizActivity.kt
+++ app/src/main/java/com/chrisvasqm/androidbasics/FourthQuizActivity.kt
@@ -2,29 +2,31 @@ package com.chrisvasqm.androidbasics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.chrisvasqm.androidbasics.databinding.ActivityFourthQuizBinding
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 class FourthQuizActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityFourthQuizBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fourth_quiz)
+        binding = ActivityFourthQuizBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        btnPreviousFourth.setOnClickListener { finish() }
-//        btnSubmit.setOnClickListener {
-//            checkAnswer()
-//        }
+        binding.btnPreviousFourth.setOnClickListener { showThirdQuiz() }
+        binding.btnSubmit.setOnClickListener { checkAnswer() }
     }
 
     private fun checkAnswer() {
-//        if (editAndroidName.text.toString() == getString(R.string.founder_android))
-//            toast(R.string.correct)
-//        else
-//            toast(R.string.wrong)
+        if (binding.editAndroidName.text.toString() == getString(R.string.founder_android))
+            toast(R.string.correct)
+        else
+            toast(R.string.wrong)
     }
 
     private fun showThirdQuiz() {
-//        startActivity<ThirdQuizActivity>()
+        startActivity<ThirdQuizActivity>()
     }
 }
